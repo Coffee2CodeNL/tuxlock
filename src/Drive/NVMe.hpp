fbd86ad21658a5a6ea2c9c794c177191a9e594a3
Remon S <remon@dead-pixel.nl>
2018-06-04 23:53:21 +0200

    Update Copyright to include GPLv3

--- src/Drive/NVMe.hpp
+++ src/Drive/NVMe.hpp
@@ -6,7 +6,7 @@
  *     it under the terms of the GNU General Public License as published by
  *    the Free Software Foundation, either version 3 of the License, or
  *    (at your option) any later version.
- *
+ * 
  *    This program is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -16,41 +16,12 @@
  *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  *    Project           :   TuxLock
- *    File              :   CLI.hpp
+ *    File              :   NVMe.hpp
  */
 #pragma once
 
-#include <iostream>
-#include <utility>
-#include "cxxopts.hpp"
-#include "rang.hpp"
-#include "spdlog/spdlog.h"
-#include "../TuxLock.hpp"
+class NVMe {
 
-#ifdef TUXLOCK_CLI_EXEC_NAME
+};
 
-std::string getExecName() {
-    return "tuxlock-cli";
-}
-
-#endif
-
-namespace TuxLock {
-    class CLI {
-    public:
-        CLI(int argc, char **argv);
-
-        void run(int argc, char *argv[]);
-
-    private:
-        std::shared_ptr<spdlog::logger> console;
-        cxxopts::Options options = cxxopts::Options(getExecName(), "SED Utility for Linux");
-
-        void initializeOptions();
-
-        void handleException(std::exception &exception, bool showHelp = false);
-
-        TuxLock *tl;
-    };
-}
 
